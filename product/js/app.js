ce67100d80f216ccdd7bae5ff26d30a3b3e46ce2
kaeserkiev <kaeserkiev@gmail.com>
2017-03-17 15:50:25 +0200

    start project and add some files

---
+++ product/js/app.js
@@ -0,0 +1,30 @@
+//Common scripts
+
+//ie fix
+if(!(window.console && console.log)) {
+	console = {
+		log: function(){},
+		debug: function(){},
+		info: function(){},
+		warn: function(){},
+		error: function(){}
+	};
+}
+
+
+(function () {
+	
+	var $body = $('body');
+
+	/*setup sticky footer*/
+	var handler = function(){
+		var footerHeight = $('#footer').height();
+
+		$('#main-wrapper').css({'padding-bottom':footerHeight});
+		
+	};
+	$(window).on('load', handler);
+	$(window).on('resize', handler);
+
+})(jQuery);
+
