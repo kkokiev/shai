36c3d60e56494d779dd4df640838d9661712686c
kaeserkiev <kaeserkiev@gmail.com>
2017-03-18 16:20:23 +0200

    add vibe page

--- product/js/app.js
+++ product/js/app.js
@@ -15,17 +15,96 @@ if(!(window.console && console.log)) {
 (function () {
 	
 	var $body = $('body');
+	var $navBtn = $('#nav-btn');
+	var $headerNav = $('#header-nav');
 
 	/*setup sticky footer*/
-	var handler = function(){
-		var footerHeight = $('#footer').height();
+	var paddingHandler = function(){
+		var footerHeight = $('#footer').innerHeight();
 
 		$('#main-wrapper').css({'padding-bottom':footerHeight});
 		
 	};
-	$(window).on('load', handler);
-	$(window).on('resize', handler);
+	$(window).on('load', paddingHandler);
+	$(window).on('resize', paddingHandler);
 
+	/*setup mobile nav */
+	$navBtn.on('click', function(){
+		$(this).toggleClass('nav-btn_opened')
+		$headerNav.slideToggle();
+	});
+
+	var navbarHandler = function(){
+		$navBtn.removeClass('nav-btn_opened');
+		if($(window).width() > 800) {
+			$headerNav.show();
+		} else {
+			$headerNav.hide();
+		}
+	};
+
+	$(window).on('resize', navbarHandler);
+
+
+	/*setup smooth scroll*/
+	$('a[href*=\\#]:not([href=\\#])').click(function() {
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+
+			if (target.length)
+			{	
+				var $headerHeight = $('.header').innerHeight(),
+					$windowWidth = $(window).width();
+
+				$('html,body').animate({
+					scrollTop: target.offset().top - ($headerHeight)
+				}, 1000);
+				return false;
+
+				// if ($windowWidth > 1100){
+
+				// 	$('html,body').animate({
+				// 		scrollTop: target.offset().top - ($headerHeight / 2)
+				// 	}, 1000);
+				// 	return false;
+
+				// } else {
+
+				// 	$('html,body').animate({
+				// 		scrollTop: target.offset().top - ($headerHeight)
+				// 	}, 1000);
+				// 	return false;
+
+				// }
+			}
+		}
+	});
+
+
+	var $gallery = $('.gallery').masonry({
+		itemSelector: '.gallery__col',
+		columnWidth: '.gallery__col-sizer',
+		percentPosition: true
+	});
+
+	$gallery.imagesLoaded().progress( function() {
+		$gallery.masonry('layout');
+	});
+
+	$body.on('click', '.gallery__col', function(e){
+		if($(this).hasClass('gallery__col_opened')) {
+			return;
+		} else {
+			$(this).addClass('gallery__col_opened');
+			var $gallery = $('.gallery').masonry({
+				itemSelector: '.gallery__col',
+				columnWidth: '.gallery__col-sizer',
+				percentPosition: true
+			});
+		}
+	});
 
 })(jQuery);
 
